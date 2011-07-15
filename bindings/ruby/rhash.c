f88920479cb10f148d1f8d1ba06a0f401f76ced0
Serguey G Basalaev <sbasalaev@gmail.com>
2011-07-15 22:53:36 +0700

    Disclaimer

--- bindings/ruby/rhash.c
+++ bindings/ruby/rhash.c
@@ -1,3 +1,20 @@
+/*
+ * Ruby Bindings for Librhash
+ * Copyright (c) 2011, Sergey Basalaev <sbasalaev@gmail.com>
+ * Librhash is (c) 2011, Alexey S Kravchenko <rhash.admin@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge,  to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction,  including without limitation the rights
+ * to  use,  copy,  modify,  merge, publish, distribute, sublicense, and/or sell
+ * copies  of  the Software,  and  to permit  persons  to whom  the Software  is
+ * furnished to do so.
+ * 
+ * This library  is distributed  in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. Use it at your own risk!
+ */
+
 #include <ruby.h>
 #include <rhash/rhash.h>
 
