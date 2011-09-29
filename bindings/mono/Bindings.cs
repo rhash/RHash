b7bb939e7da3fbb258a7b4701cef96b53385295a
Sergey Basalaev <sbasalaev@gmail.com>
2011-09-29 19:15:27 +0700

    Mono: License notices and OS-dependent library name

--- bindings/mono/Bindings.cs
+++ bindings/mono/Bindings.cs
@@ -1,3 +1,20 @@
+/*
+ * This file is a part of Mono Bindings for Librhash
+ * Copyright (c) 2011, Sergey Basalaev <sbasalaev@gmail.com>
+ * Librhash is (c) 2011, Aleksey Kravchenko <rhash.admin@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge,  to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction,  including without limitation the rights
+ * to  use,  copy,  modify,  merge, publish, distribute, sublicense, and/or sell
+ * copies  of  the Software,  and  to permit  persons  to whom  the Software  is
+ * furnished to do so.
+ * 
+ * This library  is distributed  in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. Use it at your own risk!
+ */
+
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -6,8 +23,12 @@ namespace RHash {
 	/* Pointer to native structure. */
 	sealed class Bindings {
 		
+#if WINDOWS
+		private const string librhash = "librhash.dll";
+#else
 		private const string librhash = "librhash.so.0";
-		
+#endif
+
 		private Bindings() { }
 		
 		static Bindings() {
