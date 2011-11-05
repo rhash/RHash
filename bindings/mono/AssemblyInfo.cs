9e2e6bda68c53ed438fa33ef53c32d231abbb904
Sergey Basalaev <sbasalaev@gmail.com>
2011-11-06 02:26:07 +0700

    Mono: signing with key

--- bindings/mono/AssemblyInfo.cs
+++ bindings/mono/AssemblyInfo.cs
@@ -22,6 +22,6 @@ using System.Runtime.CompilerServices;
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("rhash.snk")]
 
