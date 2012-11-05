751920758e9e7cb93fc339dfa4efd25c41779e68
Aleksey <rhash.admin@gmail.com>
2012-11-05 14:03:12 +0700

    license updated

--- librhash/rhash.c
+++ librhash/rhash.c
@@ -1,6 +1,6 @@
 /* rhash.c - implementation of LibRHash library calls
  *
- * Copyright: 2008 Aleksey Kravchenko <rhash.admin@gmail.com>
+ * Copyright: 2008-2012 Aleksey Kravchenko <rhash.admin@gmail.com>
  *
  * Permission is hereby granted,  free of charge,  to any person  obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -8,6 +8,10 @@
  * the rights to  use, copy, modify,  merge, publish, distribute, sublicense,
  * and/or sell copies  of  the Software,  and to permit  persons  to whom the
  * Software is furnished to do so.
+ *
+ * This program  is  distributed  in  the  hope  that it will be useful,  but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  Use this program  at  your own risk!
  */
 #include <string.h> /* memset() */
 #include <stdlib.h> /* free() */
