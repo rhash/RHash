f3890ce93951f606e0f5102a7f36c5dbeeac214b
Aleksey <rhash.admin@gmail.com>
2014-08-15 00:30:40 +0700

    bindings: add docstrings and remove pylint warnings

--- bindings/python/rhash.py
+++ bindings/python/rhash.py
@@ -1,19 +1,19 @@
 # Python Bindings for Librhash
 # Copyright (c) 2011-2012, Sergey Basalaev <sbasalaev@gmail.com>
 # Librhash is (c) 2011-2012, Aleksey Kravchenko <rhash.admin@gmail.com>
-# 
+#
 # Permission is hereby granted, free of charge,  to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction,  including without limitation the rights
 # to  use,  copy,  modify,  merge, publish, distribute, sublicense, and/or sell
 # copies  of  the Software,  and  to permit  persons  to whom  the Software  is
 # furnished to do so.
-# 
+#
 # This library  is distributed  in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. Use it at your own risk!
 
-'''Python bindings for librhash
+"""Python bindings for librhash
 
 Librhash  is  a library for computing and verifying hash sums
 that supports many hashing algorithms. The  simplest  way  to
@@ -34,7 +34,8 @@ compute.  The latter will return magnet  link  for the  file.
 In this function  you can  OR-combine  several hash_ids, like
 
 >>> print magnet_for_file('rhash.py', CRC32 | MD5)
-magnet:?xl=6041&dn=rhash.py&xt=urn:crc32:f5866f7a&xt=urn:md5:f88e6c1620361da9d04e2a2a1c788059
+magnet:?xl=6041&dn=rhash.py&xt=urn:crc32:f5866f7a&xt=urn:md5:
+f88e6c1620361da9d04e2a2a1c788059
 
 Next, this module provides a class to calculate several hashes
 simultaneously in an incremental way. Example of using it:
@@ -63,10 +64,11 @@ for only a single hashing algorithm.
 
 Method  magnet(filename)  will generate magnet link with  all
 hashes computed by the RHash object.
-'''
+"""
 
 # public API
-__all__ = ['ALL', 'CRC32', 'MD4', 'MD5', 'SHA1', 'TIGER', 'TTH',
+__all__ = [
+    'ALL', 'CRC32', 'MD4', 'MD5', 'SHA1', 'TIGER', 'TTH',
     'BTIH', 'ED2K', 'AICH', 'WHIRLPOOL', 'RIPEMD160', 'GOST',
     'GOST_CRYPTOPRO', 'HAS160', 'SNEFRU128', 'SNEFRU256',
     'SHA224', 'SHA256', 'SHA384', 'SHA512', 'EDONR256', 'EDONR512',
@@ -74,83 +76,92 @@ __all__ = ['ALL', 'CRC32', 'MD4', 'MD5', 'SHA1', 'TIGER', 'TTH',
     'RHash', 'hash_for_msg', 'hash_for_file', 'magnet_for_file']
 
 import sys
-from ctypes import *
+from ctypes import (
+    CDLL, c_char_p, c_int, c_size_t, c_uint, c_void_p, create_string_buffer)
 
 # initialization
 if sys.platform == 'win32':
-    libname = 'librhash.dll'
+    LIBNAME = 'librhash.dll'
 else:
-    libname = 'librhash.so.0'
-librhash = CDLL(libname)
-librhash.rhash_library_init()
+    LIBNAME = 'librhash.so.0'
+LIBRHASH = CDLL(LIBNAME)
+LIBRHASH.rhash_library_init()
 
 # function prototypes
-librhash.rhash_init.argtypes = [c_uint]
-librhash.rhash_init.restype = c_void_p
-librhash.rhash_free.argtypes = [c_void_p]
-librhash.rhash_reset.argtypes = [c_void_p]
-librhash.rhash_update.argtypes = [c_void_p, c_char_p, c_size_t]
-librhash.rhash_final.argtypes = [c_void_p, c_char_p]
-librhash.rhash_print.argtypes = [c_char_p, c_void_p, c_uint, c_int]
-librhash.rhash_print.restype = c_size_t
-librhash.rhash_print_magnet.argtypes = [c_char_p, c_char_p, c_void_p, c_uint, c_int]
-librhash.rhash_print_magnet.restype = c_size_t
-librhash.rhash_transmit.argtypes = [c_uint, c_void_p, c_size_t, c_size_t]
+LIBRHASH.rhash_init.argtypes = [c_uint]
+LIBRHASH.rhash_init.restype = c_void_p
+LIBRHASH.rhash_free.argtypes = [c_void_p]
+LIBRHASH.rhash_reset.argtypes = [c_void_p]
+LIBRHASH.rhash_update.argtypes = [c_void_p, c_char_p, c_size_t]
+LIBRHASH.rhash_final.argtypes = [c_void_p, c_char_p]
+LIBRHASH.rhash_print.argtypes = [c_char_p, c_void_p, c_uint, c_int]
+LIBRHASH.rhash_print.restype = c_size_t
+LIBRHASH.rhash_print_magnet.argtypes = [
+    c_char_p, c_char_p, c_void_p, c_uint, c_int]
+LIBRHASH.rhash_print_magnet.restype = c_size_t
+LIBRHASH.rhash_transmit.argtypes = [c_uint, c_void_p, c_size_t, c_size_t]
 
 # conversion of a string to binary data with Python 2/3 compatibility
 if sys.version < '3':
-    def b(x):
-        return x
-    def msg_to_bytes(msg):
-        if isinstance(msg, str): return msg
+    def _s2b(string):
+        """Python 2: just return the string"""
+        return string
+    def _msg_to_bytes(msg):
+        """convert the msg parameter to a string"""
+        if isinstance(msg, str):
+            return msg
         return str(msg)
 else:
     import codecs
-    def b(x):
-        return codecs.utf_8_encode(x)[0]
-    def msg_to_bytes(msg):
-        if isinstance(msg, bytes): return msg
-        if isinstance(msg, str): return b(msg)
-        return b(str(msg))
+    def _s2b(string):
+        """Python 3: convert the string to binary data"""
+        return codecs.utf_8_encode(string)[0]
+    def _msg_to_bytes(msg):
+        """convert the msg parameter to binary data"""
+        if isinstance(msg, bytes):
+            return msg
+        if isinstance(msg, str):
+            return _s2b(msg)
+        return _s2b(str(msg))
 
 # hash_id values
 CRC32 = 0x01
-MD4   = 0x02
-MD5   = 0x04
-SHA1  = 0x08
+MD4 = 0x02
+MD5 = 0x04
+SHA1 = 0x08
 TIGER = 0x10
-TTH   = 0x20
-BTIH  = 0x40
-ED2K  = 0x80
-AICH  = 0x100
+TTH = 0x20
+BTIH = 0x40
+ED2K = 0x80
+AICH = 0x100
 WHIRLPOOL = 0x200
 RIPEMD160 = 0x400
-GOST      = 0x800
+GOST = 0x800
 GOST_CRYPTOPRO = 0x1000
-HAS160    = 0x2000
+HAS160 = 0x2000
 SNEFRU128 = 0x4000
 SNEFRU256 = 0x8000
-SHA224    = 0x10000
-SHA256    = 0x20000
-SHA384    = 0x40000
-SHA512    = 0x80000
-EDONR256  = 0x100000
-EDONR512  = 0x200000
-SHA3_224  = 0x0400000
-SHA3_256  = 0x0800000
-SHA3_384  = 0x1000000
-SHA3_512  = 0x2000000
-ALL       = SHA3_512*2 - 1
+SHA224 = 0x10000
+SHA256 = 0x20000
+SHA384 = 0x40000
+SHA512 = 0x80000
+EDONR256 = 0x100000
+EDONR512 = 0x200000
+SHA3_224 = 0x0400000
+SHA3_256 = 0x0800000
+SHA3_384 = 0x1000000
+SHA3_512 = 0x2000000
+ALL = SHA3_512*2 - 1
 
 
 #rhash_print values
-RHPR_RAW    = 1
-RHPR_HEX    = 2
+RHPR_RAW = 1
+RHPR_HEX = 2
 RHPR_BASE32 = 3
 RHPR_BASE64 = 4
 RHPR_UPPERCASE = 8
 RHPR_NO_MAGNET = 0x20
-RHPR_FILESIZE  = 0x40
+RHPR_FILESIZE = 0x40
 
 class RHash(object):
     'Incremental hasher'
@@ -159,91 +170,114 @@ class RHash(object):
         if hash_ids == 0:
             self._ctx = None
             raise ValueError('Invalid argument')
-        self._ctx = librhash.rhash_init(hash_ids)
-        #switching off autofinal feature
-        librhash.rhash_transmit(5, self._ctx, 0, 0)
+        self._ctx = LIBRHASH.rhash_init(hash_ids)
+        #switching off the autofinal feature
+        LIBRHASH.rhash_transmit(5, self._ctx, 0, 0)
 
     def __del__(self):
         if self._ctx != None:
-            librhash.rhash_free(self._ctx)
+            LIBRHASH.rhash_free(self._ctx)
 
     def reset(self):
-        librhash.rhash_reset(self._ctx)
+        """reset this object to initial state"""
+        LIBRHASH.rhash_reset(self._ctx)
         return self
 
     def update(self, message):
-        data = msg_to_bytes(message)
-        librhash.rhash_update(self._ctx, data, len(data))
+        """update this object with new data chunk"""
+        data = _msg_to_bytes(message)
+        LIBRHASH.rhash_update(self._ctx, data, len(data))
         return self
 
     def __lshift__(self, message):
         return self.update(message)
 
     def update_file(self, filename):
-        f = open(filename, 'rb')
-        buf = f.read(8192)
+        """Update this object with data from the given file."""
+        file = open(filename, 'rb')
+        buf = file.read(8192)
         while len(buf) > 0:
             self.update(buf)
-            buf = f.read(8192)
-        f.close()
+            buf = file.read(8192)
+        file.close()
         return self
 
     def finish(self):
-        librhash.rhash_final(self._ctx, None)
+        """Calculate hashes for all the data buffered by
+        the update() method.
+        """
+        LIBRHASH.rhash_final(self._ctx, None)
         return self
 
     def _print(self, hash_id, flags):
+        """Retrieve the message hash in required format."""
         buf = create_string_buffer(130)
-        ln = librhash.rhash_print(buf, self._ctx, hash_id, flags)
+        size = LIBRHASH.rhash_print(buf, self._ctx, hash_id, flags)
         if (flags & 3) == RHPR_RAW:
-            return buf[0:ln]
+            return buf[0:size]
         else:
-            return buf[0:ln].decode()
+            return buf[0:size].decode()
 
-    def raw(self, hash_id = 0):
+    def raw(self, hash_id=0):
+        """Returns the message hash as raw binary data."""
         return self._print(hash_id, RHPR_RAW)
 
-    def hex(self, hash_id = 0):
+    def hex(self, hash_id=0):
+        """Returns the message hash as a hexadecimal lower-case string."""
         return self._print(hash_id, RHPR_HEX)
 
-    def HEX(self, hash_id = 0):
-        return self._print(hash_id, RHPR_HEX | RHPR_UPPERCASE)
-
-    def base32(self, hash_id = 0):
+    def base32(self, hash_id=0):
+        """Returns the message hash as a Base32 lower-case string."""
         return self._print(hash_id, RHPR_BASE32)
 
-    def BASE32(self, hash_id = 0):
-        return self._print(hash_id, RHPR_BASE32 | RHPR_UPPERCASE)
-
-    def base64(self, hash_id = 0):
+    def base64(self, hash_id=0):
+        """Returns the message hash as a Base64 string."""
         return self._print(hash_id, RHPR_BASE64)
 
-    def BASE64(self, hash_id = 0):
-        return self._print(hash_id, RHPR_BASE64 | RHPR_UPPERCASE)
+    # pylint: disable=invalid-name
+    def HEX(self, hash_id=0):
+        """Returns the message hash as a hexadecimal upper-case string."""
+        return self._print(hash_id, RHPR_HEX | RHPR_UPPERCASE)
+
+    def BASE32(self, hash_id=0):
+        """Returns the message hash as a Base32 upper-case string."""
+        return self._print(hash_id, RHPR_BASE32 | RHPR_UPPERCASE)
+    # pylint: enable=invalid-name
 
     def magnet(self, filepath):
-        ln = librhash.rhash_print_magnet(None, b(filepath), self._ctx, ALL, RHPR_FILESIZE)
-        buf = create_string_buffer(ln)
-        librhash.rhash_print_magnet(buf, b(filepath), self._ctx, ALL, RHPR_FILESIZE)
-        return buf[0:ln-1].decode('utf-8')
+        """Returns magnet link with all hashes computed by
+        this object."""
+        size = LIBRHASH.rhash_print_magnet(
+            None, _s2b(filepath), self._ctx, ALL, RHPR_FILESIZE)
+        buf = create_string_buffer(size)
+        LIBRHASH.rhash_print_magnet(
+            buf, _s2b(filepath), self._ctx, ALL, RHPR_FILESIZE)
+        return buf[0:size-1].decode('utf-8')
 
-    def hash(self, hash_id = 0):
+    def hash(self, hash_id=0):
+        """Returns the message digest for the given hash function
+        as a string in the default format."""
         return self._print(hash_id, 0)
 
     def __str__(self):
         return self._print(0, 0)
 
 def hash_for_msg(message, hash_id):
-    h = RHash(hash_id)
-    h.update(message).finish()
-    return str(h)
+    """Computes and returns the message digest (in its default format)
+    of the message"""
+    handle = RHash(hash_id)
+    handle.update(message).finish()
+    return str(handle)
 
 def hash_for_file(filename, hash_id):
-    h = RHash(hash_id)
-    h.update_file(filename).finish()
-    return str(h)
+    """Computes and returns the message digest (in its default format)
+    of the file content"""
+    handle = RHash(hash_id)
+    handle.update_file(filename).finish()
+    return str(handle)
 
 def magnet_for_file(filename, hash_mask):
-    h = RHash(hash_mask)
-    h.update_file(filename).finish()
-    return h.magnet(filename)
+    """Computes and returns the magnet link for the file."""
+    handle = RHash(hash_mask)
+    handle.update_file(filename).finish()
+    return handle.magnet(filename)
--- bindings/python/test_rhash.py
+++ bindings/python/test_rhash.py
@@ -1,93 +1,154 @@
 # UnitTest for Librhash Python Bindings
 # Copyright (c) 2011-2012, Aleksey Kravchenko <rhash.admin@gmail.com>
-# 
+#
 # Permission is hereby granted, free of charge,  to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction,  including without limitation the rights
 # to  use,  copy,  modify,  merge, publish, distribute, sublicense, and/or sell
 # copies  of  the Software,  and  to permit  persons  to whom  the Software  is
 # furnished to do so.
-# 
+#
 # This library  is distributed  in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. Use it at your own risk!
+"""Unit-tests for the rhash module"""
 
 import rhash
 import os
 import unittest
 
+# pylint: disable=too-many-public-methods, pointless-statement
 class TestRHash(unittest.TestCase):
+    """The test-case class for the rhash module"""
 
     def test_all_hashes(self):
-        r = rhash.RHash(rhash.ALL)
-        r.update('a')
-        r.finish()
-        self.assertEqual('e8b7be43', r.hash(rhash.CRC32))
-        self.assertEqual('bde52cb31de33e46245e05fbdbd6fb24', r.hash(rhash.MD4))
-        self.assertEqual('0cc175b9c0f1b6a831c399e269772661', r.hash(rhash.MD5))
-        self.assertEqual('86f7e437faa5a7fce15d1ddcb9eaeaea377667b8', r.hash(rhash.SHA1))
-        self.assertEqual('77befbef2e7ef8ab2ec8f93bf587a7fc613e247f5f247809', r.hash(rhash.TIGER))
-        self.assertEqual('czquwh3iyxbf5l3bgyugzhassmxu647ip2ike4y', r.hash(rhash.TTH))
-        self.assertEqual(40, len(r.hash(rhash.BTIH)))
-        self.assertEqual('bde52cb31de33e46245e05fbdbd6fb24', r.hash(rhash.ED2K))
-        self.assertEqual('q336in72uwt7zyk5dxolt2xk5i3xmz5y', r.hash(rhash.AICH))
-        self.assertEqual('8aca2602792aec6f11a67206531fb7d7f0dff59413145e6973c45001d0087b42d11bc645413aeff63a42391a39145a591a92200d560195e53b478584fdae231a', r.hash(rhash.WHIRLPOOL))
-        self.assertEqual('0bdc9d2d256b3ee9daae347be6f4dc835a467ffe', r.hash(rhash.RIPEMD160))
-        self.assertEqual('d42c539e367c66e9c88a801f6649349c21871b4344c6a573f849fdce62f314dd', r.hash(rhash.GOST))
-        self.assertEqual('e74c52dd282183bf37af0079c9f78055715a103f17e3133ceff1aacf2f403011', r.hash(rhash.GOST_CRYPTOPRO))
-        self.assertEqual('4872bcbc4cd0f0a9dc7c2f7045e5b43b6c830db8', r.hash(rhash.HAS160))
-        self.assertEqual('bf5ce540ae51bc50399f96746c5a15bd', r.hash(rhash.SNEFRU128))
-        self.assertEqual('45161589ac317be0ceba70db2573ddda6e668a31984b39bf65e4b664b584c63d', r.hash(rhash.SNEFRU256))
-        self.assertEqual('abd37534c7d9a2efb9465de931cd7055ffdb8879563ae98078d6d6d5', r.hash(rhash.SHA224))
-        self.assertEqual('ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb', r.hash(rhash.SHA256))
-        self.assertEqual('54a59b9f22b0b80880d8427e548b7c23abd873486e1f035dce9cd697e85175033caa88e6d57bc35efae0b5afd3145f31', r.hash(rhash.SHA384))
-        self.assertEqual('1f40fc92da241694750979ee6cf582f2d5d7d28e18335de05abc54d0560e0f5302860c652bf08d560252aa5e74210546f369fbbbce8c12cfc7957b2652fe9a75', r.hash(rhash.SHA512))
-        self.assertEqual('943aa9225a2cf154ec2e4dd81237720ba538ca8df2fd83c0b893c5d265f353a0', r.hash(rhash.EDONR256))
-        self.assertEqual('b59ec44f7beef8a04ceed38a973d77c65e22e9458d5f67b497948da34986c093b5efc5483fbee55f2f740fcad31f18d80db44bb6b8843e7fd599188e7c07233b', r.hash(rhash.EDONR512))
-        self.assertEqual('9e86ff69557ca95f405f081269685b38e3a819b309ee942f482b6a8b', r.hash(rhash.SHA3_224))
-        self.assertEqual('80084bf2fba02475726feb2cab2d8215eab14bc6bdd8bfb2c8151257032ecd8b', r.hash(rhash.SHA3_256))
-        self.assertEqual('1815f774f320491b48569efec794d249eeb59aae46d22bf77dafe25c5edc28d7ea44f93ee1234aa88f61c91912a4ccd9', r.hash(rhash.SHA3_384))
-        self.assertEqual('697f2d856172cb8309d6b8b97dac4de344b549d4dee61edfb4962d8698b7fa803f4f93ff24393586e28b5b957ac3d1d369420ce53332712f997bd336d09ab02a', r.hash(rhash.SHA3_512))
+        """Verify all hash functions"""
+        ctx = rhash.RHash(rhash.ALL)
+        ctx.update('a')
+        ctx.finish()
+        self.assertEqual('e8b7be43', ctx.hash(rhash.CRC32))
+        self.assertEqual('bde52cb31de33e46245e05fbdbd6fb24', ctx.hash(rhash.MD4))
+        self.assertEqual('0cc175b9c0f1b6a831c399e269772661', ctx.hash(rhash.MD5))
+        self.assertEqual(
+            '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8', ctx.hash(rhash.SHA1))
+        self.assertEqual(
+            '77befbef2e7ef8ab2ec8f93bf587a7fc613e247f5f247809',
+            ctx.hash(rhash.TIGER))
+        self.assertEqual(
+            'czquwh3iyxbf5l3bgyugzhassmxu647ip2ike4y', ctx.hash(rhash.TTH))
+        self.assertEqual(40, len(ctx.hash(rhash.BTIH)))
+        self.assertEqual('bde52cb31de33e46245e05fbdbd6fb24', ctx.hash(rhash.ED2K))
+        self.assertEqual('q336in72uwt7zyk5dxolt2xk5i3xmz5y', ctx.hash(rhash.AICH))
+        self.assertEqual(
+            '8aca2602792aec6f11a67206531fb7d7f0dff59413145e6973c45001d0087b42d11bc645413aeff63a42391a39145a591a92200d560195e53b478584fdae231a',
+            ctx.hash(rhash.WHIRLPOOL))
+        self.assertEqual(
+            '0bdc9d2d256b3ee9daae347be6f4dc835a467ffe', ctx.hash(rhash.RIPEMD160))
+        self.assertEqual(
+            'd42c539e367c66e9c88a801f6649349c21871b4344c6a573f849fdce62f314dd',
+            ctx.hash(rhash.GOST))
+        self.assertEqual(
+            'e74c52dd282183bf37af0079c9f78055715a103f17e3133ceff1aacf2f403011',
+            ctx.hash(rhash.GOST_CRYPTOPRO))
+        self.assertEqual(
+            '4872bcbc4cd0f0a9dc7c2f7045e5b43b6c830db8',
+            ctx.hash(rhash.HAS160))
+        self.assertEqual(
+            'bf5ce540ae51bc50399f96746c5a15bd', ctx.hash(rhash.SNEFRU128))
+        self.assertEqual(
+            '45161589ac317be0ceba70db2573ddda6e668a31984b39bf65e4b664b584c63d',
+            ctx.hash(rhash.SNEFRU256))
+        self.assertEqual(
+            'abd37534c7d9a2efb9465de931cd7055ffdb8879563ae98078d6d6d5',
+            ctx.hash(rhash.SHA224))
+        self.assertEqual(
+            'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb',
+            ctx.hash(rhash.SHA256))
+        self.assertEqual(
+            '54a59b9f22b0b80880d8427e548b7c23abd873486e1f035dce9cd697e85175033caa88e6d57bc35efae0b5afd3145f31',
+            ctx.hash(rhash.SHA384))
+        self.assertEqual(
+            '1f40fc92da241694750979ee6cf582f2d5d7d28e18335de05abc54d0560e0f5302860c652bf08d560252aa5e74210546f369fbbbce8c12cfc7957b2652fe9a75',
+            ctx.hash(rhash.SHA512))
+        self.assertEqual(
+            '943aa9225a2cf154ec2e4dd81237720ba538ca8df2fd83c0b893c5d265f353a0',
+            ctx.hash(rhash.EDONR256))
+        self.assertEqual(
+            'b59ec44f7beef8a04ceed38a973d77c65e22e9458d5f67b497948da34986c093b5efc5483fbee55f2f740fcad31f18d80db44bb6b8843e7fd599188e7c07233b',
+            ctx.hash(rhash.EDONR512))
+        self.assertEqual(
+            '9e86ff69557ca95f405f081269685b38e3a819b309ee942f482b6a8b',
+            ctx.hash(rhash.SHA3_224))
+        self.assertEqual(
+            '80084bf2fba02475726feb2cab2d8215eab14bc6bdd8bfb2c8151257032ecd8b',
+            ctx.hash(rhash.SHA3_256))
+        self.assertEqual(
+            '1815f774f320491b48569efec794d249eeb59aae46d22bf77dafe25c5edc28d7ea44f93ee1234aa88f61c91912a4ccd9',
+            ctx.hash(rhash.SHA3_384))
+        self.assertEqual(
+            '697f2d856172cb8309d6b8b97dac4de344b549d4dee61edfb4962d8698b7fa803f4f93ff24393586e28b5b957ac3d1d369420ce53332712f997bd336d09ab02a',
+            ctx.hash(rhash.SHA3_512))
         # test reset
-        self.assertEqual('d41d8cd98f00b204e9800998ecf8427e', r.reset().finish().hash(rhash.MD5)) # MD5( '' )
+        self.assertEqual(
+            'd41d8cd98f00b204e9800998ecf8427e',
+            ctx.reset().finish().hash(rhash.MD5)) # MD5( '' )
 
     def test_update(self):
-        r = rhash.RHash(rhash.CRC32 | rhash.MD5)
-        r.update('Hello, ').update('world!').finish()
-        self.assertEqual('EBE6C6E6', r.HEX(rhash.CRC32));
-        self.assertEqual('6cd3556deb0da54bca060b4c39479839', r.hex(rhash.MD5));
+        """Test the sequential update calls"""
+        ctx = rhash.RHash(rhash.CRC32 | rhash.MD5)
+        ctx.update('Hello, ').update('world!').finish()
+        self.assertEqual('EBE6C6E6', ctx.HEX(rhash.CRC32))
+        self.assertEqual('6cd3556deb0da54bca060b4c39479839', ctx.hex(rhash.MD5))
 
     def test_shift_operator(self):
-        r = rhash.RHash(rhash.MD5)
-        r << 'a' << 'bc'
-        self.assertEqual('900150983cd24fb0d6963f7d28e17f72', str(r.finish())) # MD5( 'abc' )
+        """Test the << operator"""
+        ctx = rhash.RHash(rhash.MD5)
+        ctx << 'a' << 'bc'
+        # MD5( 'abc' )
+        self.assertEqual('900150983cd24fb0d6963f7d28e17f72', str(ctx.finish()))
 
     def test_hash_for_msg(self):
-        self.assertEqual('900150983cd24fb0d6963f7d28e17f72', rhash.hash_for_msg('abc', rhash.MD5))
+        """Test the hash_for_msg() function"""
+        self.assertEqual(
+            '900150983cd24fb0d6963f7d28e17f72',
+            rhash.hash_for_msg('abc', rhash.MD5))
 
     def test_output_formats(self):
-        r = rhash.RHash(rhash.MD5 | rhash.TTH).finish()
-        self.assertEqual('5d9ed00a030e638bdb753a6a24fb900e5a63b8e73e6c25b6', r.hex(rhash.TTH))
-        self.assertEqual('2qoyzwmpaczaj2mabgmoz6ccpy', r.base32(rhash.MD5))
-        self.assertEqual('1B2M2Y8AsgTpgAmY7PhCfg==', r.base64(rhash.MD5))
-        self.assertEqual(b'\xd4\x1d\x8c\xd9\x8f\x00\xb2\x04\xe9\x80\x09\x98\xec\xf8\x42\x7e', r.raw(rhash.MD5))
+        """Test all output formats of a message digest"""
+        ctx = rhash.RHash(rhash.MD5 | rhash.TTH).finish()
+        self.assertEqual(
+            '5d9ed00a030e638bdb753a6a24fb900e5a63b8e73e6c25b6',
+            ctx.hex(rhash.TTH))
+        self.assertEqual('2qoyzwmpaczaj2mabgmoz6ccpy', ctx.base32(rhash.MD5))
+        self.assertEqual('1B2M2Y8AsgTpgAmY7PhCfg==', ctx.base64(rhash.MD5))
+        self.assertEqual(
+            b'\xd4\x1d\x8c\xd9\x8f\x00\xb2\x04\xe9\x80\x09\x98\xec\xf8\x42\x7e',
+            ctx.raw(rhash.MD5))
 
     def test_magnet(self):
-        r = rhash.RHash(rhash.MD5 | rhash.TTH)
-        r.update('abc').finish()
-        self.assertEqual('magnet:?xl=3&dn=file.txt&xt=urn:md5:900150983cd24fb0d6963f7d28e17f72&xt=urn:tree:tiger:asd4ujseh5m47pdyb46kbtsqtsgdklbhyxomuia', r.magnet('file.txt'))
+        """Test calculation of a magnet link"""
+        ctx = rhash.RHash(rhash.MD5 | rhash.TTH)
+        ctx.update('abc').finish()
+        self.assertEqual(
+            'magnet:?xl=3&dn=file.txt&xt=urn:md5:900150983cd24fb0d6963f7d28e17f72&xt=urn:tree:tiger:asd4ujseh5m47pdyb46kbtsqtsgdklbhyxomuia',
+            ctx.magnet('file.txt'))
 
     def test_update_file(self):
+        """Test the update_file() method"""
         path = 'python_test_input_123.txt'
-        f = open(path, 'wb')
-        f.write(b"\0\1\2\n")
-        f.close()
+        file = open(path, 'wb')
+        file.write(b"\0\1\2\n")
+        file.close()
 
-        r = rhash.RHash(rhash.SHA1)
-        r.update_file(path).finish()
-        self.assertEqual('e3869ec477661fad6b9fc25914bb2eee5455b483', str(r))
-        self.assertEqual('e3869ec477661fad6b9fc25914bb2eee5455b483', rhash.hash_for_file(path, rhash.SHA1))
-        self.assertEqual('magnet:?xl=4&dn=python_test_input_123.txt&xt=urn:tree:tiger:c6docz63fpef5pdfpz35z7mw2iozshxlpr4erza', rhash.magnet_for_file(path, rhash.TTH))
+        ctx = rhash.RHash(rhash.SHA1)
+        ctx.update_file(path).finish()
+        self.assertEqual('e3869ec477661fad6b9fc25914bb2eee5455b483', str(ctx))
+        self.assertEqual(
+            'e3869ec477661fad6b9fc25914bb2eee5455b483',
+            rhash.hash_for_file(path, rhash.SHA1))
+        self.assertEqual(
+            'magnet:?xl=4&dn=python_test_input_123.txt&xt=urn:tree:tiger:c6docz63fpef5pdfpz35z7mw2iozshxlpr4erza',
+            rhash.magnet_for_file(path, rhash.TTH))
         os.remove(path)
 
 if __name__ == '__main__':
