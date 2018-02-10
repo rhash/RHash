587fe10b907fa0463fca2b4641d2daed123c30e4
Aleksey <rhash.admin@gmail.com>
2018-02-10 20:16:46 +0300

    librhash: accept openssl compiled without ripemd-160

--- librhash/plug_openssl.c
+++ librhash/plug_openssl.c
@@ -20,32 +20,37 @@
 #include <openssl/opensslconf.h>
 #include <openssl/md4.h>
 #include <openssl/md5.h>
-#include <openssl/ripemd.h>
 #include <openssl/sha.h>
 
-#ifndef OPENSSL_NO_WHIRLPOOL
-#include <openssl/whrlpool.h>
-#define USE_OPENSSL_WHIRLPOOL
-#define PLUGIN_WHIRLPOOL RHASH_WHIRLPOOL
+#ifndef OPENSSL_NO_RIPEMD
+#  include <openssl/ripemd.h>
+#  define PLUGIN_RIPEMD160 RHASH_RIPEMD160
 #else
-#define PLUGIN_WHIRLPOOL 0
+#  define PLUGIN_RIPEMD160 0
+#endif
+#ifndef OPENSSL_NO_WHIRLPOOL
+#  include <openssl/whrlpool.h>
+#  define PLUGIN_WHIRLPOOL RHASH_WHIRLPOOL
+#else
+#  define PLUGIN_WHIRLPOOL 0
 #endif
 
 #include "algorithms.h"
 #include "plug_openssl.h"
 
 #if defined(OPENSSL_RUNTIME)
-# if(defined(_WIN32) || defined(__CYGWIN__))
-#  include <windows.h>
-# else
-#  include <dlfcn.h>
-# endif
+#  if(defined(_WIN32) || defined(__CYGWIN__))
+#    include <windows.h>
+#  else
+#    include <dlfcn.h>
+#  endif
 #endif
 
 #define OPENSSL_DEFAULT_HASH_MASK (RHASH_MD5 | RHASH_SHA1 | \
 	RHASH_SHA224 | RHASH_SHA256 | RHASH_SHA384 | RHASH_SHA512)
-#define PLUGIN_SUPPORTED_HASH_MASK (PLUGIN_WHIRLPOOL | RHASH_MD5 | \
-	RHASH_SHA1 | RHASH_SHA224 | RHASH_SHA256 | RHASH_SHA384 | RHASH_SHA512)
+#define PLUGIN_SUPPORTED_HASH_MASK (RHASH_MD4 | RHASH_MD5 | RHASH_SHA1 | \
+	RHASH_SHA224 | RHASH_SHA256 | RHASH_SHA384 | RHASH_SHA512 | \
+	PLUGIN_RIPEMD160 | PLUGIN_WHIRLPOOL)
 
 /* the mask of ids of hashing algorithms to use from the OpenSSL library */
 unsigned rhash_openssl_hash_mask = OPENSSL_DEFAULT_HASH_MASK;
@@ -53,7 +58,7 @@ unsigned openssl_available_algorithms_hash_mask = 0;
 
 #ifdef OPENSSL_RUNTIME
 typedef void (*os_fin_t)(void*, void*);
-#define OS_METHOD(name) os_fin_t p##name##_final = 0
+#  define OS_METHOD(name) os_fin_t p##name##_final = 0
 OS_METHOD(MD4);
 OS_METHOD(MD5);
 OS_METHOD(RIPEMD160);
@@ -64,12 +69,13 @@ OS_METHOD(SHA384);
 OS_METHOD(SHA512);
 OS_METHOD(WHIRLPOOL);
 
-#define CALL_FINAL(name, result, ctx) p##name##_final(result, ctx)
+#  define CALL_FINAL(name, result, ctx) p##name##_final(result, ctx)
+#  define HASH_INFO_METHODS(name) 0, 0, wrap##name##_Final, 0
 
-#define HASH_INFO_METHODS(name) 0, 0, wrap##name##_Final, 0
-#else /* non-runtime linking */
-#define CALL_FINAL(name, result, ctx) name##_Final(result, ctx)
-#define HASH_INFO_METHODS(name) (pinit_t)name##_Init, (pupdate_t)name##_Update, wrap##name##_Final, 0
+#else
+/* for load-time linking */
+#  define CALL_FINAL(name, result, ctx) name##_Final(result, ctx)
+#  define HASH_INFO_METHODS(name) (pinit_t)name##_Init, (pupdate_t)name##_Update, wrap##name##_Final, 0
 #endif
 
 
@@ -85,15 +91,17 @@ OS_METHOD(WHIRLPOOL);
 
 WRAP_FINAL(MD4)
 WRAP_FINAL(MD5)
-WRAP_FINAL(RIPEMD160)
 WRAP_FINAL2(SHA1, SHA_CTX)
 WRAP_FINAL2(SHA224, SHA256_CTX)
 WRAP_FINAL(SHA256)
 WRAP_FINAL2(SHA384, SHA512_CTX)
 WRAP_FINAL(SHA512)
 
+#ifndef OPENSSL_NO_RIPEMD
+  WRAP_FINAL(RIPEMD160)
+#endif
+#ifndef OPENSSL_NO_WHIRLPOOL
 /* wrapping WHIRLPOOL_Final requires special attention. */
-#ifdef USE_OPENSSL_WHIRLPOOL
 static void wrapWHIRLPOOL_Final(void* ctx, unsigned char* result)
 {
 	/* must pass NULL as the result argument, otherwise ctx will be zeroed */
@@ -109,13 +117,16 @@ rhash_hash_info rhash_openssl_methods[] = {
 	{ &info_md4, sizeof(MD4_CTX), offsetof(MD4_CTX, A), HASH_INFO_METHODS(MD4) }, /* 128 bit */
 	{ &info_md5, sizeof(MD5_CTX), offsetof(MD5_CTX, A), HASH_INFO_METHODS(MD5) }, /* 128 bit */
 	{ &info_sha1, sizeof(SHA_CTX), offsetof(SHA_CTX, h0),  HASH_INFO_METHODS(SHA1) }, /* 160 bit */
-	{ &info_rmd160, sizeof(RIPEMD160_CTX), offsetof(RIPEMD160_CTX, A), HASH_INFO_METHODS(RIPEMD160) }, /* 160 bit */
 	{ &info_sha224, sizeof(SHA256_CTX), offsetof(SHA256_CTX, h), HASH_INFO_METHODS(SHA224) }, /* 224 bit */
 	{ &info_sha256, sizeof(SHA256_CTX), offsetof(SHA256_CTX, h), HASH_INFO_METHODS(SHA256) }, /* 256 bit */
 	{ &info_sha384, sizeof(SHA512_CTX), offsetof(SHA512_CTX, h), HASH_INFO_METHODS(SHA384) }, /* 384 bit */
 	{ &info_sha512, sizeof(SHA512_CTX), offsetof(SHA512_CTX, h), HASH_INFO_METHODS(SHA512) }, /* 512 bit */
-#ifdef USE_OPENSSL_WHIRLPOOL
-	/* note: should be flags=0 */
+#ifndef OPENSSL_NO_RIPEMD
+	{ &info_rmd160, sizeof(RIPEMD160_CTX), offsetof(RIPEMD160_CTX, A), HASH_INFO_METHODS(RIPEMD160) }, /* 160 bit */
+#else
+	{ 0, 0, 0, 0, 0, 0, 0},
+#endif
+#ifndef OPENSSL_NO_WHIRLPOOL
 	{ &info_sslwhpl, sizeof(WHIRLPOOL_CTX), offsetof(WHIRLPOOL_CTX, H.c), HASH_INFO_METHODS(WHIRLPOOL) }, /* 512 bit */
 #endif
 };
@@ -175,27 +186,30 @@ static int load_openssl_runtime(void)
 	LOAD_ADDR(0, MD4)
 	LOAD_ADDR(1, MD5);
 	LOAD_ADDR(2, SHA1);
-	LOAD_ADDR(3, RIPEMD160);
-	LOAD_ADDR(4, SHA224);
-	LOAD_ADDR(5, SHA256);
-	LOAD_ADDR(6, SHA384);
-	LOAD_ADDR(7, SHA512);
-#ifdef USE_OPENSSL_WHIRLPOOL
+	LOAD_ADDR(3, SHA224);
+	LOAD_ADDR(4, SHA256);
+	LOAD_ADDR(5, SHA384);
+	LOAD_ADDR(6, SHA512);
+#ifndef OPENSSL_NO_RIPEMD
+	LOAD_ADDR(7, RIPEMD160);
+#endif
+#ifndef OPENSSL_NO_WHIRLPOOL
 	LOAD_ADDR(8, WHIRLPOOL);
 #endif
 	return 1;
 }
-#endif
+#endif /* OPENSSL_RUNTIME */
 
 /**
  * Replace several RHash internal algorithms with the OpenSSL ones.
- * It replaces MD4/MD5, SHA1/SHA2, RIPEMD, WHIRLPOOL.
+ * It can replace MD4/MD5, SHA1/SHA2, RIPEMD, WHIRLPOOL.
  *
  * @return 1 on success, 0 if OpenSSL library not found
  */
 int rhash_plug_openssl(void)
 {
-	int i, bit_index;
+	size_t i;
+	unsigned bit_index;
 
 	assert(rhash_info_size <= RHASH_HASH_COUNT); /* buffer-overflow protection */
 
@@ -213,8 +227,8 @@ int rhash_plug_openssl(void)
 	for (i = 0; i < (int)(sizeof(rhash_openssl_methods) / sizeof(rhash_hash_info)); i++)
 	{
 		rhash_hash_info *method = &rhash_openssl_methods[i];
-		if ((rhash_openssl_hash_mask & method->info->hash_id) == 0) continue;
 		if (!method->init) continue;
+		if ((rhash_openssl_hash_mask & method->info->hash_id) == 0) continue;
 		bit_index = rhash_ctz(method->info->hash_id);
 		assert(method->info->hash_id == rhash_openssl_hash_info[bit_index].info->hash_id);
 		memcpy(&rhash_openssl_hash_info[bit_index], method, sizeof(rhash_hash_info));
