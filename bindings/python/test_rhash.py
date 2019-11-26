798a09e96ed77a271f8bb0146b8a666fdb0d71ea
Aleksey <rhash.admin@gmail.com>
2019-11-27 01:43:02 +0300

    license: polish copyright notice

--- bindings/python/test_rhash.py
+++ bindings/python/test_rhash.py
@@ -1,5 +1,5 @@
 # UnitTest for Librhash Python Bindings
-# Copyright (c) 2011-2012, Aleksey Kravchenko <rhash.admin@gmail.com>
+# Copyright (c) 2011, Aleksey Kravchenko <rhash.admin@gmail.com>
 #
 # Permission is hereby granted, free of charge,  to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
