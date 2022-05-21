f1379e4eedc2fbe553a2ec47af823497bdc6fe1f
Aleksey Kravchenko <rhash.admin@gmail.com>
2022-05-22 02:17:56 +0300

    librhash: Runtime loading of crypto-1.1 library on MSYS2/CYGWIN

--- librhash/plug_openssl.c
+++ librhash/plug_openssl.c
@@ -196,18 +196,31 @@ rhash_hash_info rhash_updated_hash_info[RHASH_HASH_COUNT];
 static int load_openssl_runtime(void)
 {
 #if defined(_WIN32) || defined(__CYGWIN__)
-	HMODULE handle;
+	HMODULE handle = 0;
+	size_t i;
+
+# if defined(_WIN32)
+	static const char* libNames[] = {
+		"libeay32.dll",
+	};
+# elif defined(__MSYS__) /*  MSYS also defines __CYGWIN__ */
+	static const char* libNames[] = {
+		"msys-crypto-1.1.dll",
+		"msys-crypto-1.0.0.dll",
+	};
+# elif defined(__CYGWIN__)
+	static const char* libNames[] = {
+		"cygcrypto-1.1.dll",
+		"cygcrypto-1.0.0.dll",
+	};
+# endif
 	/* suppress the error popup dialogs */
 	UINT oldErrorMode = SetErrorMode(SEM_FAILCRITICALERRORS);
 	SetErrorMode(oldErrorMode | SEM_FAILCRITICALERRORS);
 
-# if defined(_WIN32)
-	handle = LoadLibraryA("libeay32.dll");
-# elif defined(__MSYS__) /*  MSYS also defines __CYGWIN__ */
-	handle = LoadLibraryA("msys-crypto-1.0.0.dll");
-# elif defined(__CYGWIN__)
-	handle = LoadLibraryA("cygcrypto-1.0.0.dll");
-# endif
+	for (i = 0; !handle && i < (sizeof(libNames) / sizeof(*libNames)); i++)
+		handle = LoadLibraryA(libNames[i]);
+
 	SetErrorMode(oldErrorMode); /* restore error mode */
 #else
 	static const char* libNames[] = {
